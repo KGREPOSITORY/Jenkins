96cef03162a768a3f6d7776976cc2b324723c64d
Kirill <kg2forwork@gmail.com>
2021-10-21 17:12:31 +0300

    test message

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,6 +3,8 @@ import org.testng.annotations.Test;
 
 
 public class FirstTest {
+
+
     @Test
     public void firstTest(){
         Assert.assertEquals(System.getProperty("testsResults"), "passed");
